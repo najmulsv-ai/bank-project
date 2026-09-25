e8a9e7907f4484b4ecfe6183e1b793381214c6a5
Najmul <najmulsv@gmail.com>
2026-09-24 21:52:38 -0400

    update 3

--- bank.sh
+++ bank.sh
@@ -1,7 +1,7 @@
 
 
 
-balance=1000
+balance=$(cat balance.txt)
 
 echo "1. Deposit"
 echo "2. Withdraw"
@@ -11,10 +11,21 @@ echo "4. Exit"
 read -p "Select an option " option;
 
 case $option in 
-1) echo "Deposit section";;
-2) echo "Withdraw section";;
-3) echo "Current section";;
+1) read -p "Enter deposit amount " amount
+balance=$((balance+amount)) 
+echo $balance >balance.txt
+echo "Deposit successful."
+echo "Update balance: $balance"
+;;
+
+2) read -p "Enter withdrawal amount " amount
+balance=$((balance-amount)) 
+echo $balance >balance.txt
+echo "Withdrawal successful."
+echo "Update balance: $balance"
+echo "Withdrawal section";;
+
+3) echo "Current section: $balance";;
 4) echo "Good Bye";;
 *) echo "Invalid option";; 
 esac
- 
