c4d377d4676ae66ab8243a1c7869f65eb899a7e5
Najmul <najmulsv@gmail.com>
2026-09-24 21:15:58 -0400

    test commit

--- bank.sh
+++ bank.sh
@@ -1,1 +1,20 @@
 
+
+
+balance=1000
+
+echo "1. Deposit"
+echo "2. Withdraw"
+echo "3. Current Balance"
+echo "4. Exit"
+
+read -p "Select an option " option;
+
+case $option in 
+1) echo "Deposit section";;
+2) echo "Withdraw section";;
+3) echo "Current section";;
+4) echo "Good Bye";;
+*) echo "Invalid option";; 
+esac
+ 
